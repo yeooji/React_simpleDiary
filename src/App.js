01856e98aa93697c877c9e26aeeeadcb66007b80
DESKTOP-LP3QEN4\Administrator <yeooji@naver.com>
2022-02-16 14:34:53 +0900

    [20220216] 배열 사용하기1 - 리스트 렌더링(조회)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+
+const dummyList = [
+  {
+    id: 1,
+    author: "더미1",
+    content: "내용11111",
+    emotion: 2,
+    create_date: new Date().getTime()
+  },
+  {
+    id: 2,
+    author: "더미2",
+    content: "내용222221",
+    emotion: 1,
+    create_date: new Date().getTime()
+  },
+  {
+    id: 3,
+    author: "더미3",
+    content: "333311111",
+    emotion: 3,
+    create_date: new Date().getTime()
+  }
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor/>
+      <DiaryList diaryList={dummyList} />
     </div>
   );
-}
+};
 
 export default App;
