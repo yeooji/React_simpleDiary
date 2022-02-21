0e11728cf9179ec53306d1017a1d05fcc669f2ad
DESKTOP-LP3QEN4\Administrator <yeooji@naver.com>
2022-02-21 17:20:20 +0900

    [20220221] Lifecycle 제어하기 - useEffect

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
+import Lifecycle from './Lifecycle';
 
 function App() {
   const [data, setData] = useState([]); 
@@ -35,6 +36,7 @@ function App() {
 
   return (
     <div className="App">
+      <Lifecycle/>
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
